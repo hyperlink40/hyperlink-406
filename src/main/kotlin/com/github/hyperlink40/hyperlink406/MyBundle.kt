6916513eaa1391c0dd32eaf4508ecdd0abc34be1
GitHub Action <action@github.com>
2025-05-31 21:53:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/hyperlink40/hyperlink406/MyBundle.kt
+++ src/main/kotlin/com/github/hyperlink40/hyperlink406/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hyperlink40.hyperlink406
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hyperlink40/hyperlink406/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/hyperlink40/hyperlink406/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.hyperlink40.hyperlink406.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
